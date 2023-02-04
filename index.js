f2b30da3b52ca74f6c8410ec204bae99d3e45f04
Huzaifa <huzaifa.m2199@gmail.com>
2023-02-05 03:34:51 +0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,46 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+let conversion = {
+    "PKR": {
+        "USD": 0.0044,
+        "GBP": 0.0037,
+        "PKR": 1
+    },
+    "GBP": {
+        "USD": 1.21,
+        "PKR": 271.79,
+        "GBP": 1
+    },
+    "USD": {
+        "PKR": 225.50,
+        "GBP": 0.83,
+        "USD": 1
+    },
+};
+const answers = await inquirer.prompt([
+    {
+        type: "list",
+        name: "from",
+        choices: ["PKR", "GBP", "USD"],
+        message: "Select currency"
+    },
+    {
+        type: "list",
+        name: "to",
+        choices: ["PKR", "GBP", "USD"],
+        message: "Select conversion currency"
+    },
+    {
+        type: "number",
+        name: "amount",
+        message: "Enter your amount"
+    },
+]);
+const { from, to, amount } = answers;
+if (from && to && amount) {
+    let result = conversion[from][to] * amount;
+    console.log(`Your conversion from ${from} to ${to} is ${result}`);
+}
+else {
+    console.log("Invalid Input");
+}
